edb09eef1f9ae5bb14ee091fd62bf30c4f485372
sanyath007 <sanyath007@gmail.com>
2024-10-18 17:14:19 +0700

    reflactor(VehicleList): add Loading and re-design vehicle's image and no

--- src/views/Vehicle/index.js
+++ src/views/Vehicle/index.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import { Breadcrumb, Col, Row } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import { getVehicles } from '../../features/slices/vehicleSlice'
+import Loading from '../../components/Loading'
 
 const VehicleList = () => {
     const dispatch = useDispatch();
@@ -27,13 +28,29 @@ const VehicleList = () => {
             <h3 className="font-bold text-lg mb-2">รถยนต์</h3>
 
             <Row>
-                {isLoading && <div>Loading...</div>}
+                {isLoading && <div className="text-center"><Loading /></div>}
                 {(!isLoading && vehicles) && vehicles.map(vehicle => (
-                    <Col key={vehicle.id}>
+                    <Col key={vehicle.id} md={6} lg={4} className="mb-2">
                         <div className="border rounded-md flex flex-col p-2">
-                            <img src="" alt="" />
+                            <div className="flex justify-center items-center h-[180px] mb-2">
+                                <img
+                                    src={`
+                                        ${vehicle.type_id === 2
+                                        ? 'https://m.carryboycanopy.com/lift-up-window/son/son_double_cab/fiberglass-pickup-truck-hardtop-canopy_toyota-hilux-revo_son_so-series_carryboy.png'
+                                        : (vehicle.owner_id === 15
+                                                ? 'https://www.headlightmag.com/hlmwp/wp-content/uploads/2018/10/commuter_my2018_001.jpg'
+                                                : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTr7ZUm__B3KwpNDm4M3geRA4wKnBsfBxePOQ&s'
+                                            )
+                                        }
+                                    `}
+                                    alt="vehicle-pic"
+                                    className="h-[100%]"
+                                />
+                            </div>
                             <div className="text-center flex flex-col items-center justify-center">
-                                <p className="border rounded-full w-[40px] h-[40px] flex items-center justify-center font-bold">{vehicle.no}</p>
+                                <p className="border rounded-full w-[50px] h-[50px] flex items-center justify-center text-3xl font-bold text-white bg-black mb-2">
+                                    {vehicle.no}
+                                </p>
                                 <p className="text-xl font-semibold">{vehicle.type?.name}</p>
                                 <p>{vehicle.owner?.name}</p>
                                 <p className="font-semibold">{vehicle.driver?.firstname} {vehicle.driver?.lastname}</p>
