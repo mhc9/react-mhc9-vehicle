9d7660956f5d0bcf1b22d9e8849056361b0c4aa2
sanyath007 <sanyath007@gmail.com>
2024-10-18 12:16:45 +0700

    reflactor(ReservationList): re-design layout and add action buttons

--- src/views/Reservation/index.js
+++ src/views/Reservation/index.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Breadcrumb, Col, Row } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
-import { FaClock, FaMapMarkerAlt, FaUser, FaUndoAlt } from "react-icons/fa";
+import { FaClock, FaInfoCircle, FaMapMarkerAlt, FaUser, FaUndoAlt } from "react-icons/fa";
 import moment from 'moment'
 import { getReservations } from '../../features/slices/reservationSlice'
 import { toLongTHDate } from '../../utils'
@@ -37,33 +37,49 @@ const ReservationList = () => {
             <Row>
                 {isLoading && <div className="text-center"><Loading /></div>}
                 {(!isLoading && reservations) && reservations.map(reservation => (
-                    <Col key={reservation.id} md={6} className="md:mb-2 max-sm:mb-2">
+                    <Col key={reservation.id} md={12} className="md:mb-2 max-sm:mb-2">
                         <div className="app-card border rounded-md p-3">
-                            <p className="flex flex-row items-center gap-1">
-                                <FaClock />
-                                <span className="ml-1">{toLongTHDate(moment(reservation.reserve_date).toDate())} {reservation.reserve_time} น.</span>
-                                <span className="flex flex-row items-center gap-1 ml-2">
-                                    <FaUser /> <span className="text-blue-700">{reservation.contact_name}</span>
-                                </span>
-                            </p>
-                            <p className="flex flex-row items-center gap-1">
-                                <FaMapMarkerAlt />
-                                <span className="text-red-700 ml-1">{reservation.destination}</span>
-                                <span className="ml-2">จำนวน <b>{reservation.passengers}</b> คน</span>
-                            </p>
-                            <p className="text-[10px] text-gray-400 font-thin flex flex-row items-center gap-1 pl-1">
-                                <FaUndoAlt /> {moment(reservation.updated_at).format('YYYY-MM-DD HH:mm')} น.
-                            </p>
-                        </div>
-                        {reservation.driver && (
-                            <>
-                                <hr />
-
-                                <div>
-                                    {/* driver and vehicle data here... */}
+                            <div className="flex flex-col">
+                                <div className="flex flex-row justify-between">
+                                    <div>
+                                        <p className="flex flex-row items-center gap-1">
+                                            <FaClock />
+                                            <span className="ml-1">{toLongTHDate(moment(reservation.reserve_date).toDate())} {reservation.reserve_time} น.</span>
+                                            <span className="flex flex-row items-center gap-1 ml-2">
+                                                <FaUser /> <span className="text-blue-700">{reservation.contact_name}</span>
+                                            </span>
+                                        </p>
+                                        <p className="flex flex-row items-center gap-1">
+                                            <FaMapMarkerAlt />
+                                            <span className="text-red-700 ml-1">{reservation.destination}</span>
+                                            <span className="ml-2">จำนวน <b>{reservation.passengers}</b> คน</span>
+                                        </p>
+                                        {reservation.remark && (
+                                            <p className="text-[10px] text-green-700 flex flex-row items-center gap-1 pl-1">
+                                                <FaInfoCircle /> <span className="text-xs font-thin">{reservation.remark}</span>
+                                            </p>
+                                        )}
+                                        <p className="text-[10px] text-gray-400 font-thin flex flex-row items-center gap-1 pl-1">
+                                            <FaUndoAlt /> {moment(reservation.updated_at).format('YYYY-MM-DD HH:mm')} น.
+                                        </p>
+                                    </div>
+                                    <div className="flex flex-col gap-1">
+                                        <button type="button" className="btn btn-danger">จ่ายงาน</button>
+                                        <button type="button" className="btn btn-secondary">ยกเลิก</button>
+                                    </div>
                                 </div>
-                            </>
-                        )}
+                            </div>
+
+                            {reservation.driver && (
+                                <>
+                                    <hr />
+
+                                    <div>
+                                        {/* driver and vehicle data here... */}
+                                    </div>
+                                </>
+                            )}
+                        </div>
                     </Col>
                 ))}
             </Row>
