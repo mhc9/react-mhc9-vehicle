04673fc6fc150c22d6459302860f9dee81aa50c1
sanyath007 <sanyath007@gmail.com>
2024-10-18 14:21:42 +0700

    reflactor: reset initial props to FullCalendar and add fetching reservations data logic and passing its as events object to Calendar in Home component

--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -1,6 +1,9 @@
 import React, { useRef } from 'react'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin  from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
+import './Calendar.css'
 
 const Calendar = ({ events=[] }) => {
     const calendarRef = useRef(null);
@@ -8,12 +11,19 @@ const Calendar = ({ events=[] }) => {
 
     return (
         <FullCalendar
-            plugins={[ dayGridPlugin ]}
-            initialView="dayGridMonth"
+            plugins={[ dayGridPlugin, timeGridPlugin, interactionPlugin ]}
+            initialView="timeGridWeek"
             locale="th"
             ref={calendarRef}
-            headerToolbar={false}
+            headerToolbar={{
+                left: 'prev,next',
+                center: 'title',
+                right: 'dayGridMonth,timeGridWeek,timeGridDay'
+            }}
+            slotMinTime="07:00:00"
+            slotMaxTime="24:00:00"
             initialEvents={events}
+            height={500}
         />
     )
 }
--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,8 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { getEvents } from '../../features/slices/reservationSlice'
 import Calendar from '../../components/Calendar'
+import Loading from '../../components/Loading'
 
 const Home = () => {
+    const dispatch = useDispatch();
+    const { reservations, isLoading } = useSelector(state => state.reservation);
+    useEffect(() => {
+        dispatch(getEvents({ url: `/api/reservations` }));
+    }, []);
+
     return (
         <div className="p-2">
             <div className="flex flex-row justify-around mb-2">
@@ -12,7 +21,16 @@ const Home = () => {
                 <Link to={'/driver'} className="btn btn-primary">คนขับ</Link>
             </div>
             <div className="pt-4">
-                <Calendar />
+                {isLoading && <div className="text-center"><Loading /></div>}
+                {(!isLoading && reservations) && (
+                    <Calendar events={reservations.map(reservation => {
+                        return {
+                            id: reservation.id,
+                            title: reservation.destination,
+                            start: `${reservation.reserve_date} ${reservation.reserve_time}`
+                        };
+                    })} />
+                )}
             </div>
         </div>
     )
