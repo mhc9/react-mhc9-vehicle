71a64197a9a9d639033210190f6f525d56bcaf2b
sanyath007 <sanyath007@gmail.com>
2024-10-18 13:23:06 +0700

    reflactor(Calendar): add additionals to FullCalendar

--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 
-const Calendar = () => {
+const Calendar = ({ events=[] }) => {
+    const calendarRef = useRef(null);
+    console.log(calendarRef.current);
+
     return (
         <FullCalendar
             plugins={[ dayGridPlugin ]}
             initialView="dayGridMonth"
+            locale="th"
+            ref={calendarRef}
+            headerToolbar={false}
+            initialEvents={events}
         />
     )
 }
