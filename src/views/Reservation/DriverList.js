c1c2d0c084887357cb2de7a1b43716b814130cc8
sanyath007 <sanyath007@gmail.com>
2024-10-21 16:30:57 +0700

    reflactor(DriverList): create internal ModalDriverAssignments and add it to interal DriverBadge with showModal local state

--- src/views/Reservation/DriverList.js
+++ src/views/Reservation/DriverList.js
@@ -1,5 +1,8 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Modal } from 'react-bootstrap';
 import { FaBus, FaTimesCircle } from "react-icons/fa";
+import moment from 'moment';
+import { useDispatch, useSelector } from 'react-redux';
 
 const DriverList = ({ assignments }) => {
     return (
@@ -18,12 +21,53 @@ const DriverList = ({ assignments }) => {
 export default DriverList
 
 const DriverBadge = ({ driver }) => {
+    const [showModal, setShowModal] = useState(false);
+
     return (
-        <span className="flex flex-row items-center gap-2 text-xs badge rounded-pill bg-success">
-            <a href="#" className="flex flex-row items-center gap-1" onClick={() => {}}>
-                <FaBus /> <span className="font-thin">{driver?.firstname}</span>
-            </a>
-            <FaTimesCircle size={'10px'} className="cursor-pointer hover:text-danger" />
-        </span>
+        <>
+            <ModalDriverAssignments
+                isShow={showModal}
+                hide={() => setShowModal(false)}
+                driver={driver}
+                date={moment()}
+            />
+
+            <span className="flex flex-row items-center gap-2 text-xs badge rounded-pill bg-success">
+                <a href="#" className="flex flex-row items-center gap-1" onClick={() => setShowModal(true)}>
+                    <FaBus /> <span className="font-thin">{driver?.firstname}</span>
+                </a>
+                <FaTimesCircle size={'10px'} className="cursor-pointer hover:text-danger" />
+            </span>
+        </>
+    )
+}
+
+const ModalDriverAssignments = ({ isShow, hide, driverId, date }) => {
+    const dispatch = useDispatch();
+    const { driver } = useSelector(state => state.driver);
+
+    useEffect(() => {
+        
+    }, [driverId, date]);
+
+    return (
+        <Modal
+            show={isShow}
+            onHide={hide}
+            size='lg'
+        >
+            <Modal.Header closeButton>
+                <Modal.Title></Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <table className="table table-bordered table-hover table-striped">
+                    {driver && driver.assignments.map(assignment => (
+                        <tr>
+                            <td></td>
+                        </tr>
+                    ))}
+                </table>
+            </Modal.Body>
+        </Modal>
     )
 }
