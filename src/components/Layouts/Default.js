8fff387a21c1c9c8fcc09325bd8f5324a36b20a4
sanyath007 <sanyath007@gmail.com>
2024-10-28 13:52:33 +0700

    reflactor(DefaultLayout): re-design header component

--- src/components/Layouts/Default.js
+++ src/components/Layouts/Default.js
@@ -1,13 +1,22 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
+import { FaUserCircle, FaChevronLeft } from 'react-icons/fa'
 
 const DefaultLayout = () => {
+    const navigate = useNavigate();
+    const location = useLocation();
+
     return (
         <div className="app">
             <header className="app-header min-h-[60px]">
                 <div className="flex flex-row items-center justify-between border min-h-[60px] px-3">
-                    <h2 className="font-bold text-xl">บริการรถยนต์</h2>
-                    <div className="border rounded-full w-[40px] h-[40px] flex items-center justify-center">ST</div>
+                    <div className="hidden max-[490px]:block cursor-pointer text-success">
+                        {location.pathname !== '/' && <FaChevronLeft size={'20px'} onClick={() => navigate(-1)} />}
+                    </div>
+                    <h2 className="font-extrabold text-xl text-success">บริการรถยนต์</h2>
+                    <div className="w-[40px] h-[40px] flex items-center justify-center text-success">
+                        <FaUserCircle size={'30px'} />
+                    </div>
                 </div>
             </header>
             <main className="p-3 min-h-[90vh] bg-white">
