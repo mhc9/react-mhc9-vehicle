a8dd5cd7a87313bf02fd43a9a14e28ecb39665f6
sanyath007 <sanyath007@gmail.com>
2024-10-28 14:25:06 +0700

    reflactor(Home/Navigation): set text color to all navigation's button links

--- src/views/Home/Navigation.js
+++ src/views/Home/Navigation.js
@@ -6,22 +6,22 @@ const Navigation = () => {
     return (
         <ul className="nav nav-pills nav-justified">
             <li className="nav-item max-[512px]:text-sm">
-                <Link to={'/reservation/add'} className="nav-link border hover:bg-zinc-200 flex flex-col md:flex-row items-center justify-center gap-1">
+                <Link to={'/reservation/add'} className="nav-link text-success border hover:bg-zinc-200 flex flex-col md:flex-row items-center justify-center gap-1">
                     <FaAddressCard /> <span>จอง<span className="max-[472px]:hidden">รถ</span></span>
                 </Link>
             </li>
             <li className="nav-item max-[512px]:text-sm">
-                <Link to={'/reservation'} className="nav-link border hover:bg-zinc-200 flex flex-col md:flex-row items-center justify-center gap-1">
+                <Link to={'/reservation'} className="nav-link text-success border hover:bg-zinc-200 flex flex-col md:flex-row items-center justify-center gap-1">
                     <FaTasks /> <span>รายการ<span className="max-[472px]:hidden">จอง</span></span>
                 </Link>
             </li>
             <li className="nav-item max-[512px]:text-sm">
-                <Link to={'/vehicle'} className="nav-link border hover:bg-zinc-200 flex flex-col md:flex-row items-center justify-center gap-1">
+                <Link to={'/vehicle'} className="nav-link text-success border hover:bg-zinc-200 flex flex-col md:flex-row items-center justify-center gap-1">
                     <FaBus /> <span>รถ<span className="max-[472px]:hidden">ยนต์</span></span>
                 </Link>
             </li>
             <li className="nav-item max-[512px]:text-sm">
-                <Link to={'/driver'} className="nav-link border hover:bg-zinc-200 flex flex-col md:flex-row items-center justify-center gap-1">
+                <Link to={'/driver'} className="nav-link text-success border hover:bg-zinc-200 flex flex-col md:flex-row items-center justify-center gap-1">
                     <FaUserTie /> คนขับ
                 </Link>
             </li>
