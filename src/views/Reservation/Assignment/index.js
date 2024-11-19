575f2ed877d4169c2ecd9838be9d14b4a7bf3f13
sanyath007 <sanyath007@gmail.com>
2024-11-19 15:08:14 +0700

    reflactor(Assignment): add error validation logic

--- src/views/Reservation/Assignment/index.js
+++ src/views/Reservation/Assignment/index.js
@@ -5,6 +5,11 @@ import * as Yup from 'yup'
 import { assign } from '../../../features/slices/reservationSlice'
 import Assign from './Assign';
 
+const assignmentSchema = (condition) => Yup.object().shape({
+    reservation_id: Yup.string().required(),
+    assignments: Yup.mixed().test('Items Count', 'ไม่พบการระบุผู้ขับและรถ', val => val.length > 0 && val.length === condition)
+});
+
 const Assignment = ({ reservation, date, isToggle, onCancel }) => {
     const dispatch = useDispatch();
 
@@ -25,6 +30,7 @@ const Assignment = ({ reservation, date, isToggle, onCancel }) => {
                 reservation_id: reservation ? reservation.id : '',
                 assignments: [],
             }}
+            validationSchema={assignmentSchema(reservation.vehicles)}
             onSubmit={handleSubmit}
         >
             {(formik) => {
@@ -38,26 +44,31 @@ const Assignment = ({ reservation, date, isToggle, onCancel }) => {
                                 } transition-all duration-500 mt-2 px-2 border-t max-lg:rounded-md
                             `}
                         >
-                            <h3 className="text-lg font-bold mt-1 underline">จ่ายงาน</h3>
-                            {Array(reservation.vehicles).fill(0).map((vehicle, index) => (
-                                <Fragment key={index}>
-                                    <div className="px-2 flex flex-row items-center gap-2">
-                                        <h3 className="font-bold w-[8%] max-lg:w-[10%]">คันที่ {index+1}.</h3>
+                            <div className={`${formik.errors.assignments ? 'border-[1px] border-red-600' : ''} rounded-md px-1 mt-1`}>
+                                <h3 className="text-lg font-bold underline">จ่ายงาน</h3>
+                                {Array(reservation.vehicles).fill(0).map((vehicle, index) => (
+                                    <Fragment key={index}>
+                                        <div className="px-2 flex flex-row items-center gap-2">
+                                            <h3 className="font-bold w-[8%] max-lg:w-[10%]">คันที่ {index+1}.</h3>
 
-                                        <Assign
-                                            reservation={reservation}
-                                            date={date}
-                                            onSubmit={(assignment) => {
-                                                formik.setFieldValue('assignments', [...formik.values.assignments, assignment]);
+                                            <Assign
+                                                reservation={reservation}
+                                                date={date}
+                                                onSubmit={(assignment) => {
+                                                    formik.setFieldValue('assignments', [...formik.values.assignments, assignment]);
 
-                                                setTimeout(() => formik.setFieldTouched('assignments', true), 1000);
-                                            }}
-                                        />
-                                    </div>
+                                                    setTimeout(() => formik.setFieldTouched('assignments', true), 1000);
+                                                }}
+                                            />
+                                        </div>
 
-                                    {index < Array(reservation.vehicles).fill(0).length -1 && <hr className="mb-1" />}
-                                </Fragment>
-                            ))}
+                                        {index < Array(reservation.vehicles).fill(0).length -1 && <hr className="mb-1" />}
+                                    </Fragment>
+                                ))}
+                            </div>
+                            {(formik.errors.assignments && formik.touched.assignments) && (
+                                <span className="text-red-500 text-sm">{formik.errors.assignments}</span>
+                            )}
 
                             <div className="flex justify-end px-2 pb-2">
                                 <button type="submit" className="btn btn-primary btn-sm mr-1">
